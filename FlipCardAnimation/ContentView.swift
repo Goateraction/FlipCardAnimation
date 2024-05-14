dfbe1c809230d0d8efbd061f194cb71617d40feb
Goateraction <goateraction@gmail.com>
2024-05-14 22:56:00 +0900

    Initial Commit

---
+++ FlipCardAnimation/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  FlipCardAnimation
+//
+//  Created by Goateraction on 5/14/24.
+//  Copyright © 2024 Goateraction. All rights reserved.
+//
+//  Github, Instagram, X 
+//  -> @Goateraction
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipCardAnimation/FlipCardAnimationApp.swift
@@ -0,0 +1,22 @@
+//
+//  FlipCardAnimationApp.swift
+//  FlipCardAnimation
+//
+//  Created by Goateraction on 5/14/24.
+//  Copyright © 2024 Goateraction. All rights reserved.
+//
+//  Github, Instagram, X 
+//  -> @Goateraction
+//
+
+
+import SwiftUI
+
+@main
+struct FlipCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
